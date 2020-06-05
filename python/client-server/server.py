5a86857320316c3949162d2b637cfc3fea94536e
FukeKazki <kazkichi0906@gmail.com>
2020-06-05 14:50:32 +0900

    Update: comment

--- python/client-server/server.py
+++ python/client-server/server.py
@@ -2,22 +2,28 @@ import socket
 import signal
 # キーボードからの[ctrl]+[c]を非同期で受付
 signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 # socket object
 # 第一引数: Address Family => IPv4
 # 第二引数: Socket Type => TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # taple型 (IPアドレス, ポート番号)
 sock.bind(("", 50000))
+# ソケットを待ち状態にする
 sock.listen()
+
 # 接続要求を受け入れる
-sock_c, addr = sock.accept()
+while True:
+    sock_c, addr = sock.accept()
+    # メッセージの送信
+    msg = "4432"
+    try:
+        sock_c.sendall(msg.encode("UTF-8"))
+    except:
+        print("sendall function failed.")
+    # 通信用ソケットを閉じる
+    sock_c.close()
 
-msg = "4432"
-
-try:
-    sock_c.sendall(msg.encode("UTF-8"))
-except:
-    print("sendall function failed.")
-
-sock_c.close()
+# 待ち受けようソケットを閉じる
 sock.close()
